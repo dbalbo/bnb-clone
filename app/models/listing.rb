d4228d22a7761afd0ffefd215d741860b1dd1150
audrie <audrie.talbot@gmail.com>
2015-07-22 14:04:45 -0700

    Passes first three specs for listing model.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   validates :name, :presence => true
   validates :location, :presence => true
+  has_many :reviews
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Listing do
+  it { should have_many :reviews}
   it { should validate_presence_of :name}
   it { should validate_presence_of :location}
 end
