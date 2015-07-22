ab31d1bfc89ed545fb03c28b6280f5fe91c1b348
audrie <audrie.talbot@gmail.com>
2015-07-22 13:59:48 -0700

    Passes second spec for listing model.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ActiveRecord::Base
+  belongs_to :user
   validates :name, :presence => true
+  validates :location, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722203850) do
+ActiveRecord::Schema.define(version: 20150722205359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20150722203850) do
     t.integer  "user_id"
     t.boolean  "amenity"
     t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "name"
+    t.string   "text"
+    t.integer  "listing_id"
+    t.integer  "user_id"
+    t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
